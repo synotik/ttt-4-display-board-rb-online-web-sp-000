8687dda01867645ab1e16d062011c3b2216d5367
Jeff Johnson <synotik@hotmail.com>
2020-09-12 07:36:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board
-  puts "   |   |   "
+  puts " #{display[0]} | #{display[1]} | #{display[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{display[3]} | #{display[4]} | #{display[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{display[6]} | #{display[7]} | #{display[8]} "
 end
 
 display_board
